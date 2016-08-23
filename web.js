52e9b0671647086a01af4ecac3317efa2e7a8f88
Scott Doxey <neogeek0101@gmail.com>
2016-08-22 23:38:50 -0400

    Fixed additional linting issues.

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+/* eslint no-console: ["error", { allow: ["warn"] }] */
+
 const restify = require('restify');
 
 const request = require('request');
@@ -10,22 +12,30 @@ const search = require('./utils/search.js');
 
 const images = require('./data/gravity-falls.json').images;
 
+const HTTP_CODE_UNAUTHORIZED = 401;
+const HTTP_CODE_INTERNAL_SERVER_ERROR = 500;
+
 server.get('/authorize', restify.queryParser(), (req, res, next) => {
 
-    request(`https://slack.com/api/oauth.access?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.params.code}`,
+    request(`https://slack.com/api/oauth.access?client_id=${process.env.CLIENT_ID}` +
+        `&client_secret=${process.env.CLIENT_SECRET}&code=${req.params.code}`,
         (err, response, body) => {
+
+            if (err) {
+
+                return res.send(HTTP_CODE_INTERNAL_SERVER_ERROR);
+
+            }
 
             const bodyJSON = JSON.parse(body);
 
             if (bodyJSON.ok) {
 
-                res.redirect('https://github.com/neogeek/giffany', next);
-
-            } else {
-
-                res.redirect('https://github.com/neogeek/giffany/issues/new?title=Issues%installing%20to%20Slack', next);
+                return res.redirect('https://github.com/neogeek/giffany', next);
 
             }
+
+            return res.redirect('https://github.com/neogeek/giffany/issues/', next);
 
         });
 
@@ -35,7 +45,7 @@ server.post('/giffany', restify.bodyParser(), (req, res) => {
 
     if (req.params.token !== process.env.SLACK_VERIFY_TOKEN) {
 
-        return res.send(401, 'Unauthorized')
+        return res.send(HTTP_CODE_UNAUTHORIZED, 'Unauthorized');
 
     }
 
@@ -43,27 +53,25 @@ server.post('/giffany', restify.bodyParser(), (req, res) => {
 
     if (results) {
 
-        res.send({
-            'response_type': 'in_channel',
+        return res.send({
             'attachments': [
                 {
                     'fallback': results.keywords.join(' '),
                     'image_url': results.url
                 }
-            ]
+            ],
+            'response_type': 'in_channel'
         });
-
-    } else {
-
-        res.send({
-            'response_type': 'ephemeral',
-            'text': `Sorry! No gif was found for *${req.params.text}*.`
-        });
-
-        console.warn(`Failed keyword search: "${req.params.text}"`);
 
     }
 
+    console.warn(`Failed keyword search: "${req.params.text}"`);
+
+    return res.send({
+        'response_type': 'ephemeral',
+        'text': `Sorry! No gif was found for *${req.params.text}*.`
+    });
+
 });
 
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT);
